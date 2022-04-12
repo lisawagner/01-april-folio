ec093f9792b11a8377fda0812871118ef4b347ad
Lisa Wagner <lisawagner.online@gmail.com>
2022-04-12 12:52:04 -0700

    added header scaffolding, added nav styles

--- frontend_react_folio/src/components/Navbar/Navbar.jsx
+++ frontend_react_folio/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { HiMenuAlt4, HiX, HiCode } from 'react-icons/hi'
 import { motion } from 'framer-motion'
 
-import { images } from '../../constants'
 import './Navbar.scss'
 
 // TODO: connect links array to CMS, make variables
@@ -15,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.logo} alt='logo'/>
+        <HiCode />
       </div>
 
       <ul className='app__navbar-links'>
--- frontend_react_folio/src/container/Header/Header.jsx
+++ frontend_react_folio/src/container/Header/Header.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
+import { images } from '../../constants'
 import './Header.scss'
 
-
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
 }
+
+const Header = () =>  (
+  <div className="app__header app__flex">
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 2 }}
+      className="app__header-info"
+    >
+      <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <div className="app__header-img">
+            <img src={images.logo} alt='logo'/>
+          </div>
+          <div style={{ marginLeft: 20 }}>
+            <p className="p-text">Web Developer</p>
+            <h1 className="head-text">Lisa Wagner</h1>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div>
+  </div>
+  )
+
 
 export default Header
